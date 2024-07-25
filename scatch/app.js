ce259b1e1003f516dacb5688972df4435eec9b62
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-07-25 21:29:46 +0530

    scatch models and config with seperation of concerns

--- scatch/app.js
+++ scatch/app.js
@@ -1,12 +1,24 @@
 const express=require('express');
 const app= express();
+const cookieParser= require('cookie-parser');
+const path= require('path');
+const db= require('./config/mongoose-connect');
+const ownersRoute=require('./routes/ownersRoute')
+const usersRoute=require('./routes//usersRoute')
+const productsRoute=require('./routes/productsRoute')
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
 
 const port=3000;
 
-app.get('/',(req,res)=>{
-    res.send('hey ');
-});
-
+// routes
+app.use('/owners',ownersRoute);
+app.use('/users',usersRoute);
+app.use('/products',productsRoute);
 
 
 app.listen(port,()=>{
